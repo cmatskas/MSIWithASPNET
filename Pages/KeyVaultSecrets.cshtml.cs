787dd58ec7972258cadc652091206ba1b66d7836
Christos Matskas <christos.matskas@microsoft.com>
2020-07-01 18:12:35 +0000

    KeyVault with MSI

---
+++ Pages/KeyVaultSecrets.cshtml.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace demo.Pages
+{
+    public class KeyVaultSecretsModel : PageModel
+    {
+        private readonly ILogger<KeyVaultSecretsModel> _logger;
+
+        public KeyVaultSecretsModel(ILogger<KeyVaultSecretsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public string SecretValue { get; set; }
+        public async Task OnGetAsync()
+        {
+            SecretValue = "Secret value is currently empty";
+
+            try
+            {
+                AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+                KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                var secret = await keyVaultClient.GetSecretAsync("https://cm-identity-kv.vault.azure.net/secrets/KVSercret")
+                        .ConfigureAwait(false);
+                SecretValue = secret.Value;
+            }
+
+            catch (KeyVaultErrorException keyVaultException)
+            {
+                SecretValue = keyVaultException.Message;
+            }
+        }
+    }
+}
